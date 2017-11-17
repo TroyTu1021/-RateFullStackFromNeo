3f8998a8a5eab8bd21c1696ae0f7db10e59bcc8d
Troy Tu <bloodytroy1021@163.com>
2017-11-17 10:32:23 +0000

    delete login

---
+++ RateFullStackFromNeo/src/main/webapp/ExchangeRate/webapp/controller/View1.controller.js
@@ -0,0 +1,123 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"QuickStartExchangeRate/util/ServiceLayer"
+], function(Controller,	ServiceLayer) {
+	"use strict";
+
+	return Controller.extend("QuickStartExchangeRate.controller.View1", {
+
+	onPressCheck: function(){
+		//alert("onPressCheck");
+		var thispage = this;
+		var url = "/exchangeRates?lastestExchangeRates=USD,GBP,ILS,CAD";
+		//var url = "https://addonexchangeratei343470trial.hanatrial.ondemand.com/addon-exchange-rate/exchangeRates?lastestExchangeRates=USD,GBP,ILS,CAD";
+				 $.ajax({
+					 type: 'GET'
+				    , url: url
+				    , success: function (result){
+						//alert (JSON.stringify(result));
+						
+						//var jsonobj = eval( "(" + result + ")" );//resultString to JsonObj
+						//value = jsonobj.value;
+						
+						//var oModel = new sap.ui.model.json.JSONModel();
+						//oModel.setData({ modelData: value });			
+						//sap.ui.getCore().setModel(oModel);
+						//oTable.setModel(oModel);
+						
+						var value = JSON.parse(result);
+						//var value = json.value;						
+
+						//alert (JSON.stringify(value));
+						//var x=new JSONModel(value);
+				
+						
+						
+						//var oModel = new JSONModel(jQuery.sap.getModulePath("sap.ui.demo.mock", value));
+						//this.getView().setModel(oModel);
+						
+						var j = 1;
+						var len = value.length;
+						var input;
+						var text;
+						for(var i =0;i<len;i++)
+						{
+							//var xx = value[i];
+							input = value[i].CurrencyCode;
+							//alert(input);
+							text = thispage.byId(j.toString());
+							j++;
+							text.setText(input);
+							input = value[i].Name;
+							text = thispage.byId(j.toString());
+							j++;
+							text.setText(input);
+							input = value[i].latestExchangeRate;
+							text = thispage.byId(j.toString());
+							j++;
+							text.setText(input);
+						}
+						
+						 sap.m.MessageToast.show("BindOdata.");
+			
+						},
+					"error": function(XMLHttpRequest, textStatus, errorThrown) {
+					}
+				});
+	},
+		GetSLDirect:function(){
+			var thispage = this;
+			var text;
+			for(var index=1;index<=12;index++)
+			{
+				text = thispage.byId(index.toString());
+				text.setText("");
+			}
+			
+			var url = "https://localhost:8443/OdataProxy/OdataServlet/SBOBobService_GetCurrencyRate";
+			//var url = "https://10.58.8.54:8888/OdataProxy/OdataServlet/SBOBobService_GetCurrencyRate";
+			//var url = "https://slcdemo.cfapps.sap.hana.ondemand.com/api_poc/api/b1s/v1/SBOBobService_GetCurrencyRate";
+			//var slurl = "https://slcdemo.cfapps.sap.hana.ondemand.com/api_poc/api/b1s/v1/Login";
+			//var Currencys= "EUR";
+    	//"Date": "20171107"
+			var Currencys = new Array();
+			Currencys[0] = "EUR";
+			Currencys[1] = "CAN";
+			//Currencys[0] = "USD";
+			//Currencys[1] = "GBP";
+			//Currencys[2] = "ILS";
+			//Currencys[3] = "CAD";
+			var date = "20171107";
+			var j=1;
+			var x;
+		//	var para;
+		
+			for(x in Currencys)
+			{	//alert(Currencys[x]);
+				var para1 = {"Currency": Currencys[x],"Date": date};
+				text = thispage.byId(j.toString());
+				text.setText(Currencys[x]);
+				j++;
+				text = thispage.byId(j.toString());
+				text.setText(Currencys[x]);
+				j++;
+				//text = thispage.byId(j.toString());
+				//text.setText(getRate(url,para));
+				//j++;
+				//var slResult = ServiceLayer.callSLbyPost(url,para);
+				//alert(slResult.value);
+				ServiceLayer.callSLbyPost(url,para1).then((function(jIndex) {
+						return function(slResult) {
+							if (slResult !== false) {
+								//alert( slResult.value);
+								text = thispage.byId(jIndex.toString());
+								text.setText(slResult.value);
+							}
+						};
+					})(j));
+					j++;
+			}
+	}
+	});
+
+});
---
+++ RateFullStackFromNeo/src/main/webapp/ExchangeRate/webapp/util/AjaxHelper.js
@@ -0,0 +1,64 @@
+sap.ui.define([
+	"QuickStartExchangeRate/util/CommonUtil"
+], function(CommonUtil) {
+	return {
+		_errorHandler: function(err, url) {
+			console.log(err.statusCode().statusText + ":" + url);
+		},
+
+		get: function(url, urlParam) {
+			return this._request(url, null, "GET");
+		},
+
+		post: function(url, data) {
+			return this._request(url, data && JSON.stringify(data), "POST");
+		},
+		postsimple: function(url, data) {
+			return this._request_simple(url, data && JSON.stringify(data), "POST");
+		},
+
+		_request: function(url, data, method) {
+
+			var that = this;
+			var token = CommonUtil.getApiToken();
+			var ajaxCall = $.ajax(url, {
+				headers: {
+					"X-Tenant-ID": "1",
+					//"Access-Control-Request-Method":"OPTIONS",
+					"SCP-Virtual-Host-Token": token
+					//"Content-Type": "text/plain; charset=utf-8"
+				},
+				method: method,
+				data: data,
+				//contentType: "application/json",
+				xhrFields: {
+					withCredentials: true
+				},
+				crossDomain: true
+			}).fail(function(err) {
+				that._errorHandler(err, this.url, true);
+			});
+
+			return ajaxCall;
+		},
+		
+				_request_simple: function(url, data, method) {
+
+			var that = this;
+			var token = CommonUtil.getApiToken();
+			var ajaxCall = $.ajax(url, {
+				method: method,
+				data: data.toString(),
+				contentType: "text/plain",
+				xhrFields: {
+					withCredentials: false
+				},
+				crossDomain: true
+			}).fail(function(err) {
+				that._errorHandler(err, this.url, true);
+			});
+
+			return ajaxCall;
+		}
+	};
+});
